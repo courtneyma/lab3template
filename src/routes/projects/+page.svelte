4f21f145eb41a83099bbd817df706ebd7c3439cc
courtney <81867675+courtneyma@users.noreply.github.com>
2026-02-26 15:28:12 -0500

    added json

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -7,7 +7,6 @@
   <title>Projects</title>
 </svelte:head>
 
-<pre>{ JSON.stringify(projects, null, "\t") }</pre>
 
 <nav>
   <a href=".">Home</a>
@@ -18,10 +17,15 @@
 </nav>
 <h1>Projects</h1>
 <div class="projects">
+  {#each projects as p}
     <article>
-        <h2>Lorem ipsum dolor sit.</h2>
-        <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi vitae, facere explicabo, iusto dolores minus quam omnis consectetur ipsum ullam beatae eaque placeat perferendis repellendus ratione impedit repellat nam atque?</p>
+      <h2>Lorem ipsum dolor sit.</h2>
+      <img src="https://vis-society.github.io/labs/2/images/empty.svg" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolor quos,
+        quod assumenda explicabo odio, nobis ipsa laudantium quas eum veritatis
+        ullam sint porro minima modi molestias doloribus cumque odit.
+      </p>
     </article>
-
+  {/each}
 </div>
