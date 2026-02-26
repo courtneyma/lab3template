df5035c069e6273891eef33ee59b5f130a651b32
courtney <81867675+courtneyma@users.noreply.github.com>
2026-02-26 15:10:06 -0500

    added everything before publishing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+  <a class="current" href=".">Home</a>
+  <a href="projects">Projects</a>
+  <a href="contact">Contact</a>
+  <a href="resume">Resume</a>
+  <a href="https://github.com/courtneyma" target="_blank">GitHub</a>
+</nav>
+<h1>Courtney</h1>
+<p>hi! I'm Courtney a current senior here at MIT studying computer science and writing. I'm currently interested in spatial sound, podcasting, and videography, and I thought I'd never say this but I'm also a bit in my spiritual era. If you know anything about tinctures you should contact me!(lowkey not joking)</p>
+<figure>
+    <img src="images/rudiesbaby.JPG" alt="I WWOOFed on a farm over IAP and this is one of the baby cows born while I was there!" width="1000" height="1500">
+    <figcaption>I WWOOFed on a farm over IAP and this is one of the baby cows born while I was there!</figcaption>
+</figure>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,31 @@
 <svelte:head>
   <title>Contact me</title>
+  <nav>
+    <a href=".">Home</a>
+    <a href="projects">Projects</a>
+    <a class="current" href="contact">Contact</a>
+    <a href="resume">Resume</a>
+    <a href="https://github.com/courtneyma" target="_blank">GitHub</a>
+  </nav>
+
+  <h1>Contact</h1>
+
+  <form action="mailto:cma26@mit.edu" method="GET">
+      <label>
+          Email:
+          <input type="email" name="email" />
+      </label>
+
+      <label>
+          Subject:
+          <input name="subject"  />
+      </label>
+
+      <label>
+          Body:
+          <textarea name="body"></textarea>
+      </label>
+
+      <button>Submit</button>
+  </form>
 </svelte:head>
