4b5d672acc78dbb595430920a2f268a638ee51be
courtney <81867675+courtneyma@users.noreply.github.com>
2026-02-26 15:55:14 -0500

    style changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,25 +13,43 @@
   <a href="resume">Resume</a>
   <a href="https://github.com/courtneyma" target="_blank">GitHub</a>
 </nav>
+
 <h1>Courtney</h1>
-<p>hi! I'm Courtney a current senior here at MIT studying computer science and writing. I'm currently interested in spatial sound, podcasting, and videography, and I thought I'd never say this but I'm also a bit in my spiritual era. If you know anything about tinctures you should contact me!(lowkey not joking)</p>
-<figure>
-    <img src="images/rudiesbaby.JPG" alt="I WWOOFed on a farm over IAP and this is one of the baby cows born while I was there!" width="500" height="750">
-    <figcaption>I WWOOFed on a farm over IAP and this is one of the baby cows born while I was there!</figcaption>
-</figure>
+
+<div class="home-layout">
+  <div class="home-main">
+    <p>
+      hi! I'm Courtney a current senior here at MIT studying computer science and writing. I'm
+      currently interested in spatial sound, podcasting, and videography, and I thought I'd never
+      say this but I'm also a bit in my spiritual era. If you know anything about tinctures you
+      should contact me!(lowkey not joking)
+    </p>
+
+    <figure>
+      <img
+        src="images/rudiesbaby.JPG"
+        alt="I WWOOFed on a farm over IAP and this is one of the baby cows born while I was there!"
+      />
+      <figcaption>
+        I WWOOFed on a farm over IAP and this is one of the baby cows born while I was there!
+      </figcaption>
+    </figure>
+  </div>
+
+  <aside class="home-aside">
+    <h2>What I’m Reading</h2>
+    <div class="reading">
+      {#each reading as item}
+        <ReadingItem data={item} />
+      {/each}
+    </div>
+  </aside>
+</div>
 
 <h2>Latest projects</h2>
 
-<div class="projects">
+<div class="projects highlights">
   {#each projects.slice(0, 3) as p}
     <Project data={p} />
-  {/each}
-</div>
-
-<h2>What I’m Reading</h2>
-
-<div class="reading">
-  {#each reading as item}
-    <ReadingItem data={item} />
   {/each}
 </div>
